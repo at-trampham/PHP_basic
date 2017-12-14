12ea3296c3e4e344e4d841d1e7282ff8c5333bed
Tram <tram.pham@asiantech.vn>
2017-12-14 09:43:53 +0700

    add gitinogre file and edit dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php 
 	$host='localhost';
 	$dbname='account';
-	$username='maitram';
-	$password='maitram';
+	$username='root';
+	$password='root';
   try {
      
      $conn = new PDO("mysql:host=$host;dbname=$dbname",$username, $password);     
